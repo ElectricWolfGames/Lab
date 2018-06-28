f74f94d90223404212f9749895df22044e1826d6
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2018-06-28 18:50:59 +0100

    Added Collection Project + Unit test project to holder any usefull code.

---
+++ eWolfLabs/Collection/CollectionUnitTests/Helpers/DataCleansingTests.cs
@@ -0,0 +1,37 @@
+﻿using Collection.Helpers;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CollectionUnitTests.Helpers
+{
+    public class DataCleansingTests
+    {
+        [Test]
+        public void ShouldRemoveAllStrings()
+        {
+            string str = "All and Everything else";
+            str = DataCleansing.RemoveAllStrings(str, new List<string>() { "and", "else" });
+
+            str.Should().Be("All  Everything ");
+        }
+
+        [TestCase("A  B", "A B")]
+        [TestCase("Words with single and      lots    of spaces", "Words with single and lots of spaces")]
+        public void ShouldRemoveDoubleSpaces(string starting, string expected)
+        {
+            string end = DataCleansing.RemoveDoubleSpaces(starting);
+
+            end.Should().Be(expected);
+        }
+
+        [Test]
+        public void ShouldRemoveManyStrings()
+        {
+            string str = "All and and and Everything else";
+            str = DataCleansing.RemoveAllStrings(str, new List<string>() { "and", "else" });
+
+            str.Should().Be("All    Everything ");
+        }
+    }
+}
---
+++ eWolfLabs/Collection/CollectionUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CollectionUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CollectionUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("58c05215-8331-472d-9182-5c831df90dd5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
