44e6ace92c2f28d661f220a82937dd0f351de734
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2019-02-17 08:52:46 +0000

    Added a simple CachedBool

---
+++ eWolfLabs/ExamplesUnitTests/BasicCode/CachedBoolTests.cs
@@ -0,0 +1,32 @@
+﻿using Examples.BasicCode;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ExamplesUnitTests.BasicCode
+{
+    public class CachedBoolTests
+    {
+        [Test]
+        public void ShouldCachedBool_BeBull()
+        {
+            CachedBool cb = new CachedBool();
+            cb.CachedValue.Should().BeNull();
+        }
+
+        [Test]
+        public void ShouldCachedBool_BeTrue()
+        {
+            CachedBool cb = new CachedBool();
+            cb.SetCache(true);
+            cb.CachedValue.Should().BeTrue();
+        }
+
+        [Test]
+        public void ShouldCachedBool_BeFalse()
+        {
+            CachedBool cb = new CachedBool();
+            cb.SetCache(false);
+            cb.CachedValue.Should().BeFalse();
+        }
+    }
+}
