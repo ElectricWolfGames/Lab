8ace76c71e9917bf85f45912ed304cd0a7ae8084
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2019-05-25 07:53:07 +0100

    Added Demo project to save out as Json

---
+++ eWolfLabs/DemoCode/SaveAsJson/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveAsJson
+{
+    public class Program
+    {
+        public class DataToSave
+        {
+            public string Name { get; set; }
+            public int Index { get; set; }
+        }
+
+        private static void Main(string[] args)
+        {
+            DataToSave dts = new DataToSave();
+            dts.Name = "MyName";
+
+            string output = JsonConvert.SerializeObject(dts);
+            string path = @"C:\GitHub\eWolfLabs\eWolfLabs\DemoCode\SaveAsJson\Data\FileSaved.json";
+
+            File.WriteAllText(path, output);
+
+            DataToSave dtl = JsonConvert.DeserializeObject<DataToSave>(output);
+        }
+    }
+}
