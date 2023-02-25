675825979e9e7f1af4e1827d7f6ad3d96c8f5d31
Karl Gillott <KarlGillott@googleMail.com>
2023-02-25 20:21:04 +0000

    OCR Test project.

---
+++ eWolfLabs/Tools/OCR/Program.cs
@@ -0,0 +1,26 @@
+﻿using IronOcr;
+
+namespace OCR
+{
+    // https://www.c-sharpcorner.com/article/ocr-using-tesseract-in-C-Sharp/
+    internal class Program
+    {
+        private static void Do()
+        {
+            var Ocr = new IronTesseract(); // nothing to configure
+            Ocr.Language = OcrLanguage.English;
+            using (var Input = new OcrInput())
+            {
+                Input.AddImage(@"E:\Projects\GitHub\eWolfLabs\eWolfLabs\Tools\OCR\TestImage.PNG");
+                var Result = Ocr.Read(Input);
+                Console.WriteLine(Result.Text);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Do();
+        }
+    }
+}
